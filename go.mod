4e41c79f5c8534baad7df9b56a368abb72cabf48
Max-IThub <Nikolaev.max05@gmail.com>
2023-12-09 01:14:49 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module base_module
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Привет, пользователь!\n")
+	})
+
+	port := 8080
+	fmt.Printf("Сервер запущен на порту %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
